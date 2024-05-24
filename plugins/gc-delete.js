9d696f95335dc2a48d86d4926bdbb3d5af6e6574
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-05-25 00:30:42 +0300

    Add files via upload

--- plugins/gc-delete.js
+++ plugins/gc-delete.js
@@ -1,24 +1,23 @@
 /* Creditos a https://github.com/FG98F */
 
-const handler = async (m, {conn, usedPrefix, command}) => {
-  if (!m.quoted) throw `*[❗تنبيه❗] يجب الرد على الرسالة التي ترغب في حذفها*`;
-  try {
-    const delet = m.message.extendedTextMessage.contextInfo.participant;
-    const bang = m.message.extendedTextMessage.contextInfo.stanzaId;
-    return conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
-  } catch {
-    return conn.sendMessage(m.chat, {delete: m.quoted.vM.key});
-  }
-};
-handler.help = ['del', 'delete'];
-handler.tags = ['group'];
-handler.command = /^حذف?$/i;
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-export default handler;
+let handler = async (m, { conn, usedPrefix, command }) => {	
+if (!m.quoted) throw `*❮ ❗┇يجب ان تضع ريبلاي للرسالة الذي تريد حذفها❯*`
+try {
+let delet = m.message.extendedTextMessage.contextInfo.participant
+let bang = m.message.extendedTextMessage.contextInfo.stanzaId
+return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+} catch {
+return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+}}
+handler.help = ['del', 'delete']
+handler.tags = ['group']
+handler.command = /^احذف(ete)?$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+export default handler
 
-/* let handler = function (m) {
+/*let handler = function (m) {
 if (!m.quoted) throw false
 let { chat, fromMe, isBaileys } = m.quoted
 if (!fromMe) throw false
