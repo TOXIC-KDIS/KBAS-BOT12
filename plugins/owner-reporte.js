e0f8c8b3678b656a0a69664f0ae5f5ac180f2f96
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-05-24 19:41:45 +0300

    Update owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] أدخل التقرير*\n\n*مثال:*\n*${usedPrefix + command} الأمر ${usedPrefix} عين مش شغال*`;
-  if (text.length < 10) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚁𝙴𝙿𝙾𝚁𝚃𝙴 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 𝙳𝙴 𝙼𝙸𝙽𝙸𝙼𝙾 𝟷0 𝙲𝙰𝚁𝙰𝙲𝚃𝙴𝚁𝙴𝚂!*`;
-  if (text.length > 1000) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚁𝙴𝙿𝙾𝚁𝚃𝙴 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 𝙳𝙴 𝙼𝙰𝚇𝙸𝙼𝙾 𝟷000 𝙲𝙰𝚁𝙰𝙲𝚃𝙴𝚁𝙴𝚂!*`;
+  if (text.length < 10) throw `*[❗𝐈𝐍𝐅𝐎❗] يجب أن يتكون التقرير من 10 أحرف على الأقل!*`;
+  if (text.length > 1000) throw `*[❗𝐈𝐍𝐅𝐎❗] يجب أن يكون التقرير بحد أقصى 1000 حرف!*`;
   const teks = `*❒═════[𝐑𝐄𝐏𝐎𝐑𝐓𝐄]═════❒*\n*┬*\n*├❧ 𝙽𝚄𝙼𝙴𝚁𝙾:* wa.me/${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ 𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${text}\n*┴*`;
   conn.reply('967773289295@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   conn.reply('967773289295@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
