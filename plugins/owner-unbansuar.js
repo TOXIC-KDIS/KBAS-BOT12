0b288f2639d3ff101fe897793a0d78d810b8411a
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-05-26 20:07:11 +0300

    Update owner-unbansuar.js

--- plugins/owner-unbansuar.js
+++ plugins/owner-unbansuar.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text}) => {
-if (!text) throw '*[❗] متنساش المنشن يحب*'
+if (!text) throw '*[❗] لاتنسى المنشن يحب*'
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-if (!who) throw '*[❗] متنساش المنشن يحب*'
+if (!who) throw '*[❗] لاتنسى المنشن يحب*'
 let users = global.db.data.users
 users[who].banned = false
-conn.reply(m.chat, `*[❗]تم إلغاء حظر المستخدم*\n*—◉ يقدر يستخدم البوت دلوقت*`, m)
+conn.reply(m.chat, `*[❗]تم إلغاء حظر المستخدم*\n*—◉ يقدر يستخدم البوت هاذا الوقت*`, m)
 }
 handler.help = ['unbanuser']
 handler.tags = ['owner']
