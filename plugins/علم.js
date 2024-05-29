008eb1b980f0ca5b6c65985fef9b25bcfc0e07d3
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-05-29 15:39:42 +0300

    Add files via upload

--- plugins/علم.js
+++ plugins/علم.js
@@ -1,3 +1,8 @@
+//BY KUROSAKI 
+//كود من طرف كوروساكي 
+//لينك القنات : https://whatsapp.com/channel/0029VaSJqny2kNFyDSt8dM2r
+//+212669512443
+//🧧🗿🗿🗿🗿🗿
 let timeout = 60000
 let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
@@ -7,25 +12,24 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://gist.githubusercontent.com/YosefZoro1/2e1ba4a3f14368c5620ef099ac21bcdb/raw/294a1b3e4114f4937382a285534bdcacc06917de/game6.json')).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/Hjfjckb/Kurosaki/main/Kurosaki.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*${command.toUpperCase()}*
   ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
   *استخدم .انسحب للأنسحاب*
   ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-『𝙕𝙊𝙍𝙊-𝘽𝙊𝙏』
      `.trim()
     conn.tebakbendera[id] = [
-        await conn.sendFile(m.chat, json.img, '', caption, m),
+        await conn.sendFile(m.chat, json.question, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.response}*┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
 }
 handler.help = ['guessflag']
 handler.tags = ['game']
-handler.command = /^عين/i
+handler.command = /^علم/i
 
 export default handler
