aebcfcfe1b8d680c860c487d38b157858c32f994
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-06-04 16:23:10 +0300

    Update مينو زورو.js

--- plugins/مينو زورو.js
+++ plugins/مينو زورو.js
@@ -249,7 +249,7 @@ global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remot
 }
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = ['الاوامر', 'المهام','اوامر','الأوامر'] 
+handler.command = ['', '','','حخ'] 
 
 export default handler
 function clockString(ms) {
